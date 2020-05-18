d686aca23f81502615ca9b0a9f427197abe99eb1
BilyKing <64271272+BilyKing@users.noreply.github.com>
2020-05-18 17:56:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git openwrt-packages https://github.com/BilyKing/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git openwrt-packages https://github.com/BilyKing/openwrt-packages' feeds.conf.default
 
